da69e990dbc78464df795f8ab5073f05caa2bc3c
puneetmpatil <patilpuneet01@gmail.com>
2023-05-16 01:23:59 +0530

    About,Contact,Home,Services(Slider is not showing), new routes added

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
 db = PyMongo(app).db
 
 @app.route("/")
-def home_page():
-    db.inventory.insert_one({"c":2})
-    a = db.inventory.find({})
-    for item in a:
-        print(item)
-    return f"<p>Helllo World</p>"
-
-@app.route("/home")
 def home():
+    # db.inventory.insert_one({"c":2})
+    # a = db.inventory.find({})
+    # for item in a:
+        # print(item)
+    # return f"<p>Hello World</p>"
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
